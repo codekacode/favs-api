47dea7a1c364e8e2971e6e3069f2290ad422ed51
Codekacode <karem.quiroz.m@gmail.com>
2022-04-18 21:40:04 -0500

    Connecta database

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
 export const createDbConnection = (dbURL: string) => {
-    mongoose
-        .connect(dbURL)
+    mongoose.connect(dbURL)
         .then(() => {
             console.log('Base de datos conectada a la app');
         })
         .catch((error) => {
             console.log('Error en la conexion de datos')
         });
+    mongoose.connection.on('error',() => console.log('Error en la conexion de datos DB conection'));
+    mongoose.connection.once('connected', () => console.log('db connected'));
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
-import dotenv from 'dotenv';
 import { createDbConnection } from './config/databaseConfig';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
