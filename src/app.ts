d5a91376df685d99a724696c4512cc72ada4077d
Codekacode <karem.quiroz.m@gmail.com>
2022-04-29 17:14:33 -0500

    add validation middlewares and auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
-import userRoutes from './users/routes/userRoutes'
-import listRoutes from './lists/routes/listRoutes'
+import userRoutes from './users/routes/userRoutes';
+import listRoutes from './lists/routes/listRoutes';
+import authRoutes from './auth/router/authRouter';;
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(userRoutes);
 app.use(listRoutes);
+app.use(authRoutes)
 
 export default app;
